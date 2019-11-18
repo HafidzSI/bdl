4924dc2328317abec38cb6a0e54c9c45f33689f1
HafidzSI <37611838+HafidzSI@users.noreply.github.com>
2019-11-19 01:32:17 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 define('db_host', 'localhost');
 define('db_user', 'root');
-define('db_pass', 'root');
+define('db_pass', 'mypassword');
 define('db_name', 'paketwisata');
-define('db_port', 3307);
+define('db_port', 3306);
 
 $conn = new mysqli(db_host, db_user, db_pass, db_name, db_port);
 if ($conn->connect_errno > 0) {
